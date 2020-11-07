fcf2d3f110a9fe4f99913101c23d2f1c413f21b3
Emma <69841693+xentraz@users.noreply.github.com>
2020-11-07 19:05:35 +0100

    navbar update

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidebar-nav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("sidebar-nav").style.width = "0%";
+}
+
+// https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp //
